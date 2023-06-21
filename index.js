22c766ce730afb5c844110f9e66f523e63a7040c
JotaJotaM1 <johnjames_89@hotmail.com>
2023-06-21 00:47:56 -0500

    challenge finished

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const form = document.getElementById('myForm');
 const emailInput = document.getElementById('email');
 const contenedor1 = document.getElementById('contenedor1');
 const contenedor2 = document.getElementById('contenedor2');
+const dismissButton = document.querySelector('.buttonTwo');
+const mainContent = document.querySelector('main');
 
 function validateEmail() {
   const email = emailInput.value;
@@ -26,13 +28,29 @@ function handleSubmit(event) {
     contenedor1.style.display = "none";
     contenedor2.style.display = "flex";
   } else {
-    // El correo electrónico es inválido, mostrar un mensaje de error
+    alert("Ingresa un correo electrónico válido");
     console.log('Correo electrónico inválido');
   }
 }
 
+// Función para ocultar el contenedor2 y mostrar el contenedor1
+function dismissMessage() {
+  contenedor1.style.display = "flex";
+  contenedor2.style.display = "none";
+}
+
 // Agregar el evento de envío del formulario
 form.addEventListener('submit', handleSubmit);
+
+// Función para ocultar el contenedor2 y mostrar el main
+function handleDismiss() {
+    contenedor2.style.display = 'none';
+    mainContent.style.display = 'block';
+  }
+  
+// Agregar el evento de clic al botón "Dismiss message"
+dismissButton.addEventListener('click', handleDismiss);
+
 
 
 
